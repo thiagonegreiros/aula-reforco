19aabf88c229d5560f8933980f1838b6ae16783a
douglas-emeefe <douglasoliveira.main@gmail.com>
2024-03-05 22:45:44 -0400

    Endpoint de criar, filtrar

--- backend/src/routes/student.routes.ts
+++ backend/src/routes/student.routes.ts
@@ -1,9 +1,26 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { StudentHandler } from "../handlers/students.handler";
 
 const studentsRoutes = new Elysia({ prefix: "/student" });
 const studentHandler = new StudentHandler();
 
 studentsRoutes.get("/", studentHandler.findAll());
+studentsRoutes.get("/:id", async({params})=>{
+    const findById = await studentHandler.findById(Number(params.id));
+    return findById;
+});
+
+studentsRoutes.post("/", async({body})=>{
+    const add = await studentHandler.add(body);
+    return {
+        response: "Usuário Cadastrado - "+add.name,
+    };
+},{
+    body:t.Object({
+        name: t.String(),
+        responsable_name: t.String(),
+        responsable_number: t.String(),
+    })
+});
 
 export { studentsRoutes };
