548ce57a6a0708fcf4b841d5e6ca715fa80a4b0c
Thiago Negreiros <negreiros.t@gmail.com>
2024-03-21 00:37:14 -0300

    feat: add scrollview

--- mobile/src/app/Profile.tsx
+++ mobile/src/app/Profile.tsx
@@ -1,7 +1,7 @@
 import { Badge } from "@/components/Badge";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/Card";
 import { User } from "@/components/User";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 export function Profile() {
   return (
@@ -9,62 +9,64 @@ export function Profile() {
       <View className="flex-1 px-4 pb-4 mt-6">
         <User />
 
-        <Card className="bg-white mb-4 mt-4">
-          <CardHeader>
-            <View className="flex-row">
-              <Badge key="1" label="Matemagica" />
-            </View>
-          </CardHeader>
-          <CardContent>
-            <Text className="font-bold text-xl">Derivadas e Integrais</Text>
-            <Text>
-              Integration and derivation are fundamental concepts in calculus,
-              enabling us to understand the relationship between a function and
-              its rate of change or area under the curve.
-            </Text>
-          </CardContent>
-          <CardFooter>
-            <Text>Data da Lição: 19/03/2024</Text>
-          </CardFooter>
-        </Card>
+        <ScrollView>
+          <Card className="bg-white mb-4 mt-4">
+            <CardHeader>
+              <View className="flex-row">
+                <Badge key="1" label="Matemagica" />
+              </View>
+            </CardHeader>
+            <CardContent>
+              <Text className="font-bold text-xl">Derivadas e Integrais</Text>
+              <Text>
+                Integration and derivation are fundamental concepts in calculus,
+                enabling us to understand the relationship between a function
+                and its rate of change or area under the curve.
+              </Text>
+            </CardContent>
+            <CardFooter>
+              <Text>Data da Lição: 19/03/2024</Text>
+            </CardFooter>
+          </Card>
 
-        <Card className="bg-white mb-4">
-          <CardHeader>
-            <View className="flex-row">
-              <Badge key="1" label="Matemagica" />
-            </View>
-          </CardHeader>
-          <CardContent>
-            <Text className="font-bold text-xl">Derivadas e Integrais</Text>
-            <Text>
-              Integration and derivation are fundamental concepts in calculus,
-              enabling us to understand the relationship between a function and
-              its rate of change or area under the curve.
-            </Text>
-          </CardContent>
-          <CardFooter>
-            <Text>Data da Lição: 19/03/2024</Text>
-          </CardFooter>
-        </Card>
+          <Card className="bg-white mb-4">
+            <CardHeader>
+              <View className="flex-row">
+                <Badge key="1" label="Matemagica" />
+              </View>
+            </CardHeader>
+            <CardContent>
+              <Text className="font-bold text-xl">Derivadas e Integrais</Text>
+              <Text>
+                Integration and derivation are fundamental concepts in calculus,
+                enabling us to understand the relationship between a function
+                and its rate of change or area under the curve.
+              </Text>
+            </CardContent>
+            <CardFooter>
+              <Text>Data da Lição: 19/03/2024</Text>
+            </CardFooter>
+          </Card>
 
-        <Card className="bg-white">
-          <CardHeader>
-            <View className="flex-row">
-              <Badge key="1" label="Matemagica" />
-            </View>
-          </CardHeader>
-          <CardContent>
-            <Text className="font-bold text-xl">Derivadas e Integrais</Text>
-            <Text>
-              Integration and derivation are fundamental concepts in calculus,
-              enabling us to understand the relationship between a function and
-              its rate of change or area under the curve.
-            </Text>
-          </CardContent>
-          <CardFooter>
-            <Text>Data da Lição: 19/03/2024</Text>
-          </CardFooter>
-        </Card>
+          <Card className="bg-white">
+            <CardHeader>
+              <View className="flex-row">
+                <Badge key="1" label="Matemagica" />
+              </View>
+            </CardHeader>
+            <CardContent>
+              <Text className="font-bold text-xl">Derivadas e Integrais</Text>
+              <Text>
+                Integration and derivation are fundamental concepts in calculus,
+                enabling us to understand the relationship between a function
+                and its rate of change or area under the curve.
+              </Text>
+            </CardContent>
+            <CardFooter>
+              <Text>Data da Lição: 19/03/2024</Text>
+            </CardFooter>
+          </Card>
+        </ScrollView>
       </View>
     </View>
   );
