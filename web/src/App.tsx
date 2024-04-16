b4ec5765ffee47cecc1f18853b1495b5d4fde0a2
Thiago Negreiros <negreiros.t@gmail.com>
2024-04-15 22:18:03 -0300

    feat: config shadcnui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+import "./global.css";
+
 export function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
+  );
 }
